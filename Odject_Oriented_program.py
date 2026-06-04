dcf4eca8d220836f0cddbdea3456eb4b2c12dc0a
VIGNESHN877 <vigneshn877@gmail.com>
2026-06-04 16:50:52 +0530

    this is a object_oriented_programming first function test

---
+++ Odject_Oriented_program.py
@@ -0,0 +1,25 @@
+# object Oriented Program 
+
+no_of_wheels=4
+mileage=20.9
+no_of_airbages=5
+
+
+def moveforword():
+    print("Car is Moving !")
+      
+
+def movebackword():
+    print("Car is moving backword ! ")
+
+
+# copy of the number of templete so that called for a class
+
+#  That are created for a called of the class that name insantest 
+
+# class is a template
+#object   is a class copy
+
+
+movebackword()
+moveforword()
