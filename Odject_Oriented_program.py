cd03df54f579af5955f4388b957e58fc3bf779a8
VIGNESHN877 <vigneshn877@gmail.com>
2026-06-04 21:22:00 +0530

    class function method asscess to the a class example two

--- Odject_Oriented_program.py
+++ Odject_Oriented_program.py
@@ -1,4 +1,4 @@
-# object Oriented Program 
+"""# object Oriented Program 
 
 no_of_wheels=4
 mileage=20.9
@@ -60,4 +60,34 @@ car2.no_of_wheels=8
 
 print("\n","This is your car mileage :",car2.mileage,"\n","this is your car airbages : ",car2.no_of_airbages,"\n","this is your car wheels :",car2.no_of_wheels)
 print("\n","This is your car mileage :",car1.mileage,"\n","this is your car airbages : ",car1.no_of_airbages,"\n","this is your car wheels :",car1.no_of_wheels)
+
+"""
+
+
+# create the a class value in the function in the bank account 
+
+class Bank_account():
+    customer_name=input("Enter Your name : ")
+    balance=float(input("this is your account balance :"))
+    account_number=int(input("Enter the your account number :"))
+
+customer3=Bank_account()
+customer3.customer_name
+customer3.balance
+customer3.account_number
+
+customer1=Bank_account()
+customer1.customer_name="Dinesh kumar . S"
+customer1.balance=145000
+customer1.account_number=214406933
+
+customer2=Bank_account()
+customer2.customer_name="siva"
+customer2.balance=100000.50
+customer2.account_number=121210650
+
+print("\n",customer3.customer_name,"\n",customer3.balance,"\n",customer3.account_number,"\n")
+print("\n",customer1.customer_name,"\n",customer1.balance,"\n",customer1.account_number,"\n")
+print("\n",customer2.customer_name,"\n",customer2.balance,"\n",customer2.account_number,"\n")
+
 
