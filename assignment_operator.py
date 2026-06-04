9aefd2b13b59bdae1279f0f509e836c9b48d6721
VIGNESHN877 <vigneshn877@gmail.com>
2026-06-04 16:16:34 +0530

    added for a assignment operator file updata with the function

--- assignment_operator.py
+++ assignment_operator.py
@@ -6,10 +6,38 @@ Available for work :
 rating :
 
 '''
+def add(a,b):
+    return a+b
 
+def sum(a,b):
+    return a-b
+
+def mulit(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+def reminder(a,b):
+    return a%b
+
+a=int(input("Enter the A value :"))
+b=int(input("Enter the B value :"))
+print(add(a,b))
+print()
+print(sum(a,b))
+print()
+print(mulit(a,b))
+print()
+print(div(a,b))
+print()
+print(reminder(a,b))
+print()
+
+"""
 name=input("Enter Your Name :")
 Age=int(input("Enter Your Age :"))
 Work = "Available for work"
 score=float(input("Enter Your Ratings :"))
 
-print("Your Name :",name,'\n','Your Age is :',Age,'\n',"This is Your work :",Work,'\n',"This your score of Ratings:",score,'\n')
+print("Your Name :",name,'\n','Your Age is :',Age,'\n',"This is Your work :",Work,'\n',"This your score of Ratings:",score,'\n')"""
