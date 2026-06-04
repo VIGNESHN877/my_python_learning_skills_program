e0bd8075b207a9763b60c93e0f9fc51b51ebbb89
VIGNESHN877 <vigneshn877@gmail.com>
2026-06-04 16:18:20 +0530

    this modules.py file is a develop to the functions value

---
+++ modules.py
@@ -0,0 +1,22 @@
+#import assignment_operator
+from assignment_operator import add,sum,mulit,div,reminder
+
+a=int(input("Enter the A value :"))
+b=int(input("Enter the B value :"))
+
+print(add(a,b))
+print()
+print(sum(a,b))
+print()
+print(mulit(a,b))
+print()
+print(div(a,b))
+print()
+print(reminder(a,b))
+print()
+print(type(reminder))
+"""
+print(assignment_operator.add(22,22))
+
+
+print(assignment_operator.sum(22,22))"""
