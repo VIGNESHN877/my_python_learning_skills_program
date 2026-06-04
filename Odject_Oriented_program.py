9cdedbe8bccbd44de319f6acf3f8d5538f2fd300
VIGNESHN877 <vigneshn877@gmail.com>
2026-06-04 17:39:49 +0530

    this is a  car class value function assign in the value

--- Odject_Oriented_program.py
+++ Odject_Oriented_program.py
@@ -23,3 +23,41 @@ def movebackword():
 
 movebackword()
 moveforword()
+
+
+class car:
+    no_of_wheels =0
+    mileage=0.0
+    no_of_airbages=0
+
+    def moveforward(self):
+        print("car is moving !")
+
+    def movebackword(self):
+        print("Car is moving backward !")
+
+car1=car() # instance of the case :object instantion
+print(car1.mileage)
+car2=car()
+print("millage :",car2.mileage)
+car2.mileage=25.0
+print("milleage :",car2.mileage)
+car3=car()
+print(car1.mileage)
+car3=car()
+car3.moveforward()
+
+car1.mileage=25
+car1.no_of_airbages=8
+car1.no_of_wheels=8
+
+
+
+car2=car()
+car2.mileage=22
+car2.no_of_airbages=8
+car2.no_of_wheels=8
+
+print("\n","This is your car mileage :",car2.mileage,"\n","this is your car airbages : ",car2.no_of_airbages,"\n","this is your car wheels :",car2.no_of_wheels)
+print("\n","This is your car mileage :",car1.mileage,"\n","this is your car airbages : ",car1.no_of_airbages,"\n","this is your car wheels :",car1.no_of_wheels)
+
