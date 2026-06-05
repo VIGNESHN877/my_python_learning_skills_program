d675d6b393a93698b88d8269535780b85254a5f8
VIGNESHN877 <vigneshn877@gmail.com>
2026-06-05 12:32:18 +0530

    getters & setters Getters and Setters are methods used to access (get) and modify (set) the values of an object's attributes in a controlled way.
    Getter → Used to read/access a variable's value.
    Setter → Used to update/change a variable's value.

---
+++ Getters&Setters.py
@@ -0,0 +1,10 @@
+# getter 
+
+def get__no__of__wheels(self):
+    print("No of Wheels :",get__no__of__wheels)
+
+
+#setter once private value change 
+# setter or updata using for setter function
+def get_no_of_wheels(self):
+    print("No of Wheels :",get_no_of_wheels)
