a5b5b1379a3fc1a7ef9368aba9e21b267fb45afb
VIGNESHN877 <vigneshn877@gmail.com>
2026-06-05 14:00:15 +0530

    types of inheritance

--- inheritance.py
+++ inheritance.py
@@ -1,4 +1,4 @@
-#parent class
+"""#parent class
 
 
 class Animal:
@@ -16,4 +16,61 @@ object.sound()
 object.bark()
 
 
-#i exptced output is : animal makes sound , Dog bark
+
+#i exptced output is : animal makes sound , Dog bark"""
+
+
+
+"""
+type of tnheritance in python 
+1.single inhertance
+once child inherits one parent 
+
+class A :
+   pass
+class B:
+    pass
+"""
+
+"""
+2.Muliple inheritance 
+once child inherits multiple parents 
+
+class A:
+    pass
+class B:
+    pass 
+    
+class(A,B):
+    pass
+"""
+
+"""
+3.Multiple Inheritance
+
+class A:
+    pass
+class B:
+    pass
+
+class c(B):
+    pass
+    
+"""
+
+"""
+4.Hierachial Inheritance 
+
+Multiple child class inherit one perent 
+class A:
+    pass
+class B(A):
+    pass 
+class A(B):
+    pass
+
+"""
+
+"""
+5.Hybird inheritance is a cobination of the Different inheritance types
+"""
